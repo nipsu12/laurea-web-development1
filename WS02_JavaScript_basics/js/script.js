890272078bd6025774a09ca76597a1f3b987d270
Nita Lallimo <nita.lallimo@gmail.com>
2026-09-17 09:56:58 +0300

    tarkistettuohjeet

--- WS02_JavaScript_basics/js/script.js
+++ WS02_JavaScript_basics/js/script.js
@@ -1,32 +1,36 @@
-// STEP 1: Print text to the browser console is string ""
+// Exercise 1 – Developer Tools and Console
 
 console.log("Hello, World!");
 alert("Hello, World!");
 
-// STEP 2: Variables
-
-// Store text values in variables
-const userName = "Nita";
-const favoriteAnimal = "Leopardi";
-
-// Print the variables to the console
-console.log("My name is " + userName);
-console.log("My favorite animal is " + favoriteAnimal);
-
-// STEP 3: User interaction
-
 // Display a pop-up message
 alert("Tervetuloa hienolle nettisivulleni!");
 
+// Exercise 2 – Variables
 
-// Ask the user for their name
+const userName = "Nita";
+let age = 22;
+const favoriteAnimal = "Leopardi";
+    
+// Print the variables to the console
+console.log("My name is " + userName);
+console.log("I'm " + age + " years old.");
+console.log("My favorite animal is " + favoriteAnimal);
+
+// Create a sentence using two answers
+console.log("Hello! My name is " + userName + " and my favorite animal is " + favoriteAnimal + ".");
+
+// Exercise 3 – User Input
+
+// Ask the user for their name with prompt
 const visitorName = prompt("Mikä on nimesi?");
 
 // Print the user's answer
 console.log(visitorName);
 
-console.log("Hello, " + visitorName + "!");
+console.log("Hello, " + visitorName + "! Welcome to JavaScript.");
 
+// Exercise 4 – Conditionals
 // ask the user for their age
 const visitorAge = prompt("Kuinka vanha olet?");
 
@@ -36,17 +40,19 @@ if (visitorAge >= 18) {
     console.log("You are under 18.");
 }
 
+// Exercise 5 – Functions
+// function called greetuser receives name as parameter and prints a greeting
+function greetUser(visitorName) {
+    console.log("Hello, " + visitorName + "!");
+}
+
+// ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
 // Ask the user for their favorite animal
 const visitorFavoriteAnimal = prompt("Mikä on lempieläimesi?");
 
 // Create a sentence using both answers
 console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
 
-function viesti() {
-    alert("You found the message!");
-}
-
-// ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
 // ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
 
 if (visitorFavoriteAnimal === "Blue Elephant") {
@@ -55,5 +61,6 @@ if (visitorFavoriteAnimal === "Blue Elephant") {
     console.log("Nice! Your favorite animal is " + visitorFavoriteAnimal + ".");
 }
 
-
-// const visitorFavoriteAnimal = prompt("What is your favorite animal?");
+function viesti() {
+    alert("You found the message!");
+}
